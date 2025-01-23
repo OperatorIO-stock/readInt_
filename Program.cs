d6e0e080c7a6e99b5c02c86c78610daa0db2e033
OperatorIO-stock <vova.chigorin@yandex.com>
2025-01-23 20:06:13 +0300

    commit Good

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace readInt;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RequestNumber());
+    }
+
+    static int RequestNumber()
+    {
+        bool isWorkingProgram = true;
+
+        while (isWorkingProgram)
+        {
+            Console.Write("Введите число: ");
+
+            try
+            {
+                int numberUser = int.Parse(Console.ReadLine());
+                return numberUser;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Вы ввели некорректное число. Пожалуйста, попробуйте снова.");
+            }
+        }
+        
+        return 0;
+    }
+}
